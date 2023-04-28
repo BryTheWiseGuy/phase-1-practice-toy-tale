9f9ccac1be5463d2e6123a1900642005c6852e8a
Brian Ashman <brian.ashman@comcast.net>
2023-04-27 22:06:18 -0700

    Complete Toy Tale Lab

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ let addToy = false;
 document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
-  addBtn.addEventListener("click", () => {
-    // hide & seek with the form
+  addBtn.addEventListener("click", (e) => {
+    e.preventDefault();
     addToy = !addToy;
     if (addToy) {
       toyFormContainer.style.display = "block";
@@ -12,4 +12,96 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+
+  toyFormContainer.addEventListener('submit', addNewToy);
+
+  fetch('http://localhost:3000/toys')
+  .then(res => res.json())
+  .then(toys => {
+    createCard(toys);
+  })
+
+  function createCard(toys) {
+    let toyCollection = document.querySelector('#toy-collection')
+    toys.forEach((toy) => {
+      let toyCard = document.createElement('div');
+      let toyName = document.createElement('h2');
+      let toyImage = document.createElement('img');
+      let toyLikes = document.createElement('p');
+      let likeBtn = document.createElement('button');
+      toyCard.className = "card";
+      toyName.innerText = toy.name;
+      toyImage.src = toy.image;
+      toyImage.className = "toy-avatar";
+      toyLikes.innerText = toy.likes;
+      likeBtn.className = "like-btn";
+      likeBtn.id = toy.id;
+      likeBtn.innerText = "Like ❤"
+      likeBtn.addEventListener('click', () => {
+        toy.likes += 1;
+        toyLikes.innerText = toy.likes;
+        handleLikes(toy, likeBtn);
+        }
+      )
+      toyCard.appendChild(toyName);
+      toyCard.appendChild(toyImage);
+      toyCard.appendChild(toyLikes);
+      toyCard.appendChild(likeBtn);
+      toyCollection.appendChild(toyCard);
+    })
+  }
+
+  function addNewToy(e) {
+    e.preventDefault();
+    let toyObj = {
+      name: e.target.name.value,
+      image: e.target.image.value,
+      likes: 0
+    }
+    postNewToy(toyObj);
+    renderNewToyCard(toyObj);
+    e.target.reset();
+  }
+
+  function renderNewToyCard(toyObj) {
+    let toyCollection = document.querySelector('#toy-collection')
+    let toyCard = document.createElement('div');
+    let toyName = document.createElement('h2');
+    let toyImage = document.createElement('img');
+    let toyLikes = document.createElement('p');
+    let likeBtn = document.createElement('button');
+    toyCard.className = "card";
+    toyName.innerText = toyObj.name;
+    toyImage.src = toyObj.image;
+    toyImage.className = "toy-avatar";
+    toyLikes.innerText = toyObj.likes;
+    likeBtn.className = "like-btn";
+    likeBtn.id = toyObj.id;
+    likeBtn.innerText = "Like ❤"
+    toyCard.appendChild(toyName);
+    toyCard.appendChild(toyImage);
+    toyCard.appendChild(toyLikes);
+    toyCard.appendChild(likeBtn);
+    toyCollection.appendChild(toyCard);
+    }
+
+  function postNewToy (toyObj) {
+    fetch('http://localhost:3000/toys', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(toyObj)
+    })
+  }
+
+  function handleLikes (toyObj, likeBtn) {
+    fetch(`http://localhost:3000/toys/${likeBtn.id}`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(toyObj)
+    })
+  }
 });
